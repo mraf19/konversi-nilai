bd034d7a1e55b333fd6a1d6e8b5cfd6a8a180136
mraf19 <raflimr1902@gmail.com>
2021-10-23 19:43:42 +0700

    completing project

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let check = true
 function cekNilai (nilai) {
-    if (nilai !== null){
+    if (nilai !== null && nilai !== ""){
         if ( nilai <=100 && nilai >= 90){
             alert("Grade A")
-        } else if (nilai < 90 && nilai >= 85){
+        } else if (nilai < 90 && nilai >= 75){
             alert("Grade B")
-        }else if (nilai < 85 && nilai >= 60) {
+        }else if (nilai < 75 && nilai >= 55) {
             alert("Grade C")
-        }else if (nilai < 60 && nilai >= 40){
+        }else if (nilai < 55 && nilai >= 30){
             alert("Grade D")
-        }else if (nilai < 40 && nilai >= 0){
+        }else if (nilai < 30 && nilai >= 0){
             alert("Grade E")
         }else {
             alert("Nilai yang anda Masukkan tidak valid. Masukkan nilai dengan rentang dari 0 sampai dengan 100")
